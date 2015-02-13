586e3a57336884adf052be3bcfd343cec7e390d6
Ryan Eballar <ryeballar@gmail.com>
2015-02-13 13:54:05 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('james', function() {
+	return gulp.src('assets/scss/*.scss')
+		.pipe(sass())
+		.on('error', function handleError(err) {
+			console.error(err.toString());
+			this.emit('end');
+		})
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('assets/css'));
+});
